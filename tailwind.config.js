5df6ef24b654971042660764dd21c52a853dd9ad
all glenn <88910044+allglenn@users.noreply.github.com>
2022-04-03 23:06:34 +0200

    index html

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily : {
+        "title" : ['Roboto Mono','sans-serif'],
+        "body" : ['Lato','sans-serif']
+      },
+      colors : {
+        "pri": '#00254d',
+        "sec": '#0ea8a1',
+        "bac": '#ffff',
+        "gen": '#3b4d71',
+      }
+    },
+  },
+  plugins: [],
+} 
